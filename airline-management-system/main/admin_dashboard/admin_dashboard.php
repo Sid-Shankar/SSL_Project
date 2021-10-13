e66d16b21ce26b9a4a9374af3711b66da3fb12cc
niteeshchaudhary <nkchaudhary00@gmail.com>
2021-10-13 13:31:14 +0530

    Update admin_dashboard.php

--- airline-management-system/main/admin_dashboard/admin_dashboard.php
+++ airline-management-system/main/admin_dashboard/admin_dashboard.php
@@ -16,29 +16,253 @@ if (isset($_SESSION["admin_id"])) {
 }
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
 
-
-
-<html>
 <head>
-<title>Welcome to admin dashboard</title>
-<link href="assets/css/style.css" type="text/css"
-	rel="stylesheet" />
-<link href="assets/css/admin_login_signup.css" type="text/css"
-	rel="stylesheet" />
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Airline management system</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons are not needed (that logo on tab name)-->
+  
+
+  <!-- Google Fonts -->
+  <link
+    href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+    rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="../assets_welcome_page/vendor/aos/aos.css" rel="stylesheet">
+  <link href="../assets_welcome_page/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="../assets_welcome_page/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="../assets_welcome_page/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="../assets_welcome_page/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="../assets_welcome_page/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="../assets_welcome_page/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="../assets_welcome_page/css/style.css" rel="stylesheet">
+
+
+  <style>
+
+    #header .logo {
+      font-size: 20px;
+      margin: 0;
+      padding: 0;
+      line-height: 1;
+      font-weight: 450;
+      letter-spacing: 2px;
+      text-transform: uppercase;
+    }
+
+    #header .logo img {
+      max-height: 60px;
+    }
+
+  </style>
+
 </head>
+
 <body>
-	<div class="ams-container">
-		<div class="page-header">
-			<span class="login-signup"><a href="../admin_login/admin_logout.php">Click here to Logout</a></span>
-		</div>
-		<div class="page-content">Welcome <?php echo $admin_id;?></div>
-        <br><br>
-        <div class="page-content">Note:1) Please use Admin dashboard page instead of this page.</div>
-        <br> <br>
-        <div class="page-content">2)Please use admin_logout.php for sign out option in admin_dashboard</div>
-        <br><br>
-        <div class="page-content"><b>3)Work in Progress ...<b>  </div>
-	</div>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top ">
+    <div class="container d-flex align-items-center">
+
+      <a href="../index.html" class="logo me-auto"><img src="../assets_welcome_page/img/iitdh_logo.png" alt="" class="img-fluid"></a>
+      <h1 class="logo me-auto"><a href="index.html">Airline Mgmt System</a></h1>
+
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+          <li><a class="nav-link scrollto" href="#about">About Us</a></li>
+          <li><a class="nav-link scrollto" href="../process_login_signup.php">Book now</a></li>
+          <li><a class="nav-link scrollto" href="../search_flights.html">Search flights</a></li>
+          <li><a class="nav-link scrollto" href="#contact">Contact Us</a></li>
+          <li><a class="nav-link scrollto" href="#faq">FAQ</a></li>
+          <li><a class="getstarted scrollto" href="../admin_login/admin_logout.php">Logout</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+<div style="display:block;height: 100px;width:100%;background-color: blue;"></div>
+  <div class="ams-container">
+    <div class="page-content">Welcome <?php echo $admin_id;?></div>
+</div>
+<main id="main">
+    <!-- ======= Dashboard Section ======= -->
+    <section id="services" class="services section-bg">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Dash Board</h2>
+          <p>.............</p>
+        </div>
+
+        <div class="row">
+          <div class="col-xl-3 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bxl-dribbble"></i></div>
+              <h4><a href="">Add a new flight</a></h4>
+              <p>Add a new Flights for Passengers and delight them by your services.</p>
+            </div>
+          </div>
+
+          <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-aos="zoom-in"
+            data-aos-delay="200">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-file"></i></div>
+              <h4><a href="">Update an existing flight</a></h4>
+              <p>Make changes to previously added flights.</p>
+            </div>
+          </div>
+
+          <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in"
+            data-aos-delay="300">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-tachometer"></i></div>
+              <h4><a href=""> Remove an existing flight</a></h4>
+              <p>Cancel the flight scheduled.</p>
+            </div>
+          </div>
+
+          <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in"
+            data-aos-delay="400">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-layer"></i></div>
+              <h4><a href="">Update reservation status</a></h4>
+              <p>make changes to reservation status</p>
+            </div>
+          </div>
+
+        </div>
+
+<br>
+<br>
+
+        <div class="row">
+          <div class="col-xl-3 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bxl-dribbble"></i></div>
+              <h4><a href="">View all flights</a></h4>
+              <p>Have a look on all the added flights available.</p>
+            </div>
+          </div>
+
+          <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-aos="zoom-in"
+            data-aos-delay="200">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-file"></i></div>
+              <h4><a href="">View Booked flights</a></h4>
+              <p>View all the booked flights by passengers.</p>
+            </div>
+          </div>
+
+          <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in"
+            data-aos-delay="300">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-tachometer"></i></div>
+              <h4><a href="">Delete a passenger record</a></h4>
+              <p>Remove the record of passenger with cancelled booking.</p>
+            </div>
+          </div>
+
+          <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in"
+            data-aos-delay="400">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-layer"></i></div>
+              <h4><a href="">View all passenger record</a></h4>
+              <p>Passengers who have booked the filght</p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Services Section -->
+
+  </main><!-- End #main -->   
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+
+
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6 footer-contact">
+            <h3>Airline Management System</h3>
+            <p>
+              XYZ Road <br>
+              Dharwad, Karnataka 1100XX<br>
+              India <br><br>
+              <strong>Phone:</strong> +91-99999999<br>
+              <strong>Email:</strong> infoxyz@iitdh.ac.in<br>
+            </p>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Important Links</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#hero">Home</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="process_login_signup.php">Login/Signup</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#about">About us</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="process_login_signup.php">Book Now</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="search_flights.html">Search flights</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#faq">FAQ</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#contact">Contact Us</a></li>
+            </ul>
+          </div>
+
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Our Social Networks</h4>
+            <p>Connect with us through any of them</p>
+            <div class="social-links mt-3">
+              <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+              <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+              <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+              <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+              <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container footer-bottom clearfix">
+      <div class="copyright">
+        &copy; Copyright <strong><span>Airline Management system</span></strong>. All Rights Reserved
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+      class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="../assets_welcome_page/vendor/aos/aos.js"></script>
+  <script src="../assets_welcome_page/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="../assets_welcome_page/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="../assets_welcome_page/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="../assets_welcome_page/vendor/php-email-form/validate.js"></script>
+  <script src="../assets_welcome_page/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="../assets_welcome_page/vendor/waypoints/noframework.waypoints.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="../assets_welcome_page/js/main.js"></script>
+
 </body>
+
 </html>
